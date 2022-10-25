94e8dd9a8a298cb16b92981d6866a0d17e88f0db
g.mironov <devmigregal@gmail.com>
2022-10-25 22:02:31 +0300

    lab-05: init cmd

--- lab_05/pkg/digsig/keys.go
+++ lab_05/pkg/digsig/keys.go
@@ -5,10 +5,10 @@ import (
 	"crypto/rand"
 	"crypto/x509"
 	"encoding/pem"
-	"os"
+	"io"
 )
 
-func GenerateKeys(privKey, pubKey string) error {
+func GenerateKeys(privKey, pubKey io.Writer) error {
 	publicKey, privateKey, err := ed25519.GenerateKey(rand.Reader)
 	if err != nil {
 		return err
@@ -27,19 +27,13 @@ func GenerateKeys(privKey, pubKey string) error {
 	return nil
 }
 
-func generatePrivateKey(key string, privateKey ed25519.PrivateKey) error {
-	privateKeyPEM, err := os.Create(key)
-	if err != nil {
-		return err
-	}
-	defer privateKeyPEM.Close()
-
+func generatePrivateKey(out io.Writer, privateKey ed25519.PrivateKey) error {
 	privBytes, err := x509.MarshalPKCS8PrivateKey(privateKey)
 	if err != nil {
 		return err
 	}
 
-	if err := pem.Encode(privateKeyPEM, &pem.Block{
+	if err := pem.Encode(out, &pem.Block{
 		Type:  "PRIVATE KEY",
 		Bytes: privBytes,
 	}); err != nil {
@@ -49,19 +43,13 @@ func generatePrivateKey(key string, privateKey ed25519.PrivateKey) error {
 	return nil
 }
 
-func generatePublicKey(key string, publicKey ed25519.PublicKey) error {
-	publicKeyPEM, err := os.Create(key)
-	if err != nil {
-		return err
-	}
-	defer publicKeyPEM.Close()
-
+func generatePublicKey(out io.Writer, publicKey ed25519.PublicKey) error {
 	pubBytes, err := x509.MarshalPKIXPublicKey(publicKey)
 	if err != nil {
 		return err
 	}
 
-	if err := pem.Encode(publicKeyPEM, &pem.Block{
+	if err := pem.Encode(out, &pem.Block{
 		Type:  "PUBLIC KEY",
 		Bytes: pubBytes,
 	}); err != nil {
--- lab_05/pkg/digsig/sign.go
+++ lab_05/pkg/digsig/sign.go
@@ -4,24 +4,14 @@ import (
 	"crypto/ed25519"
 	"crypto/x509"
 	"encoding/pem"
-	"os"
 )
 
-func Sign(privKey, file, sig string) error {
-	data, err := os.ReadFile(file)
-	if err != nil {
-		return err
-	}
-	key, err := os.ReadFile(privKey)
-	if err != nil {
-		return err
-	}
-
+func Sign(data, key []byte) ([]byte, error) {
 	pem, _ := pem.Decode(key)
 	privKeyParsed, err := x509.ParsePKCS8PrivateKey(pem.Bytes)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return os.WriteFile(sig, ed25519.Sign(privKeyParsed.(ed25519.PrivateKey), data), 0644)
+	return ed25519.Sign(privKeyParsed.(ed25519.PrivateKey), data), nil
 }
